188a8e760b2c704cb41ea2fc621c3edfa4be83d9
Krishnapriya-2506 <skrishnapriya691@gmail.com>
2026-04-26 16:00:55 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { AppNavigator } from './src/navigation/AppNavigator';
+import { useFonts } from 'expo-font';
+import { MaterialCommunityIcons, Ionicons, FontAwesome } from '@expo/vector-icons';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    ...MaterialCommunityIcons.font,
+    ...Ionicons.font,
+    ...FontAwesome.font,
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <>
       <StatusBar style="auto" />
